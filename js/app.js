58272de4d3167b0912670d1f49e9974c055e58d4
Maciej Mandzak <maciekekseke@gmail.com>
2020-04-11 16:44:01 +0200

    Logic for objects prepared. Works :)

--- js/app.js
+++ js/app.js
@@ -16,15 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
         for(let i = 0; i<divsCountA; i++){
             let newDiv = document.createElement("div");
             this.board.appendChild(newDiv);
+            let y = Math.ceil((i+1)/this.width);
+            let x = (i+1)%this.width;
+            this.cells.push({reference:newDiv,x: x, y: y, isAlive: false, tableIndex: x*y});
         }
 
-        this.cells = document.querySelectorAll("#board div");
+/*        this.cells = document.querySelectorAll("#board div");*/
 
         this.cells.forEach(function (cell) {
-            cell.addEventListener("click", function (e) {
+            cell.reference.addEventListener("click", function (e) {
                 this.classList.toggle("live");
+                if(cell.isAlive===false){
+                    cell.isAlive=true;
+                }
+                else {
+                    cell.isAlive=false;
+                }
+                console.log(`${cell.x}, ${cell.y}`+` ` +cell.isAlive)
             })
-        })
+        });
     };
 
     let game = new GameOfLife(20,20);
